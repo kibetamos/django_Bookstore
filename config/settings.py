fd0e2a7d8d94867379fa84b209dfffd7d6823945
kibetamos <amos.rotich@strathmore.edu>
2022-11-28 18:22:39 +0300

    Update: Added the model for customer management

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Local
+    'accounts',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'accounts.CustomUser' # new
